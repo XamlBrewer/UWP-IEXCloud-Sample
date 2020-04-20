919aaddf0dd90daa33873b79b2e283d31ef14c85
Diederik Krols <diederik_krols@hotmail.com>
2020-04-20 21:58:11 +0200

    Add project files.

---
+++ XamlBrewer.UWP.IEXCloud.Sample/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.Core;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace XamlBrewer.UWP.IEXCloud.Sample
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            var coreTitleBar = CoreApplication.GetCurrentView().TitleBar;
+            coreTitleBar.ExtendViewIntoTitleBar = true;
+
+            var titleBar = ApplicationView.GetForCurrentView().TitleBar;
+            if (titleBar != null)
+            {
+                titleBar.BackgroundColor = Colors.Transparent;
+                titleBar.ButtonBackgroundColor = Colors.Transparent;
+                titleBar.ButtonInactiveBackgroundColor = Colors.SlateGray;
+                titleBar.ButtonForegroundColor = Color.FromArgb(255, 214, 117, 36);
+            }
+
+            this.InitializeComponent();
+        }
+    }
+}
