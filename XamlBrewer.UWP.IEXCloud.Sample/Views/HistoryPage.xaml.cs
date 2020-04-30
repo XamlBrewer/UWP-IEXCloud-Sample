fa7bfa9d3101e1699c0702dfbf04b206eae1ff2e
Diederik Krols <diederik_krols@hotmail.com>
2020-04-30 15:37:47 +0200

    History page with Chart

---
+++ XamlBrewer.UWP.IEXCloud.Sample/Views/HistoryPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using VSLee.IEXSharp.Model.Stock.Request;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using XamlBrewer.UWP.IEXCloud.Sample.Services;
+
+namespace XamlBrewer.UWP.IEXCloud.Sample.Views
+{
+    public sealed partial class HistoryPage : Page
+    {
+        public HistoryPage()
+        {
+            this.InitializeComponent();
+
+            Loaded += HistoryPage_Loaded;
+        }
+
+        private async void HistoryPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            ProgressRing.IsActive = true;
+
+            using (var iexCloudClient = IEXCloudService.GetClient())
+            {
+                var response = await iexCloudClient.Stock.HistoricalPriceAsync("MSFT");
+                if (response.ErrorMessage != null)
+                {
+                    Console.WriteLine(response.ErrorMessage);
+                }
+                else
+                {
+                    HistoricPrices.ItemsSource = response.Data;
+                    CandleSticks.ItemsSource = response.Data;
+                }
+            }
+
+            ProgressRing.IsActive = false;
+        }
+    }
+}
