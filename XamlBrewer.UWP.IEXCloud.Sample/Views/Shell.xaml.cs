1dce6ebcb46078216a59276e54258a63ab80faba
Diederik Krols <diederik_krols@hotmail.com>
2020-04-30 19:33:08 +0200

    Startup TeachingTip

--- XamlBrewer.UWP.IEXCloud.Sample/Views/Shell.xaml.cs
+++ XamlBrewer.UWP.IEXCloud.Sample/Views/Shell.xaml.cs
@@ -15,6 +15,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using XamlBrewer.UWP.IEXCloud.Sample.Services;
 using XamlBrewer.UWP.IEXCloud.Sample.Views;
 using WinUI = Microsoft.UI.Xaml.Controls;
 
@@ -37,6 +38,19 @@ namespace XamlBrewer.UWP.IEXCloud.Sample
             }
 
             this.InitializeComponent();
+
+            Loaded += Shell_Loaded;
+        }
+
+        private void Shell_Loaded(object sender, RoutedEventArgs e)
+        {
+            var settings = new Settings();
+            if (String.IsNullOrEmpty(settings.PublishableKey) && String.IsNullOrEmpty(settings.PublishableSandBoxKey))
+            {
+                SettingsTip.Target = NavigationView.SettingsItem as FrameworkElement;
+                SettingsTip.PreferredPlacement = WinUI.TeachingTipPlacementMode.TopLeft;
+                SettingsTip.IsOpen = true;
+            }
         }
 
         private void NavigationView_ItemInvoked(WinUI.NavigationView sender, WinUI.NavigationViewItemInvokedEventArgs args)
@@ -52,6 +66,12 @@ namespace XamlBrewer.UWP.IEXCloud.Sample
 
             ContentFrame.Navigate(Type.GetType(item.Tag.ToString()));
             NavigationView.Header = item.Content;
+        }
+
+        private void SettingsTip_CloseButtonClick(WinUI.TeachingTip sender, object args)
+        {
+            ContentFrame.Navigate(typeof(SettingsPage));
+            NavigationView.Header = "Settings";
         }
     }
 }
