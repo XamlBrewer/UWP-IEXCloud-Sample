616eef1213801530e110e8f82b85d6b1fe3687de
Diederik Krols <diederik_krols@hotmail.com>
2020-05-05 21:48:30 +0200

    Portfolio page sparkline

---
+++ XamlBrewer.UWP.IEXCloud.Sample/Models/PorfolioItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VSLee.IEXSharp.Model.Stock.Response;
+
+namespace XamlBrewer.UWP.IEXCloud.Sample.Models
+{
+    public class PorfolioItem
+    {
+        public string Symbol { get; set; }
+
+        public DateTime BuyDate { get; set; }
+
+        public int Quantity { get; set; }
+
+        public double BuyPrice { get; set; }
+
+        public IEnumerable<HistoricalPriceResponse> HistoricalPrices { get; set; }
+    }
+}
---
+++ XamlBrewer.UWP.IEXCloud.Sample/Views/PortfolioPage.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using XamlBrewer.UWP.IEXCloud.Sample.Models;
+using XamlBrewer.UWP.IEXCloud.Sample.Services;
+
+namespace XamlBrewer.UWP.IEXCloud.Sample.Views
+{
+    public sealed partial class PortfolioPage : Page
+    {
+        public PortfolioPage()
+        {
+            this.InitializeComponent();
+
+            Loaded += PortfolioPage_Loaded;
+        }
+
+        private async void PortfolioPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            //
+            // Test with one item. Grid comes later ...
+            //
+
+            var item = new PorfolioItem
+            {
+                Symbol = "MSFT",
+                BuyDate = new DateTime(2020, 1, 1),
+                Quantity = 100,
+                BuyPrice = 120
+            };
+
+            ProgressRing.IsActive = true;
+
+            var queryStringBuilder = new VSLee.IEXSharp.Helper.QueryStringBuilder();
+            queryStringBuilder.Add("chartCloseOnly", "true");
+            queryStringBuilder.Add("chartSimplify", "true");
+
+            using (var iexCloudClient = IEXCloudService.GetClient())
+            {
+                var response = await iexCloudClient.Stock.HistoricalPriceAsync(item.Symbol,VSLee.IEXSharp.Model.Stock.Request.ChartRange._3m, queryStringBuilder);
+                if (response.ErrorMessage != null)
+                {
+                    Console.WriteLine(response.ErrorMessage);
+                }
+                else
+                {
+                    item.HistoricalPrices = response.Data;
+                    VerticalAxis.Minimum = (double)item.HistoricalPrices.Select(h => h.close).Min();
+                    VerticalAxis.Maximum = (double)item.HistoricalPrices.Select(h => h.close).Max();
+                    HistoricalPrices.ItemsSource = item.HistoricalPrices;
+                }
+            }
+
+            ProgressRing.IsActive = false;
+        }
+    }
+}
